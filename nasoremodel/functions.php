2a791667c2329673b926819309ee9ed9c82ce609
jason naso <jasonnaso@jasons-MacBook-Air.local>
2015-12-24 00:26:50 +0100

    start over

--- nasoremodel/functions.php
+++
@@ -1,6 +0,0 @@
-<!-- <?php
-// function autoLogout(){
-// 		echo "<script>setTimeout(function(){ window.location.href='includes/logout.php'; }, 1200*1000); </script>";
-// 	}
-
-// ?>-->
--- nasoremodel/includes/dbConnection.php
+++
@@ -1,6 +0,0 @@
- <?php
-
-    $db = new PDO('mysql:host=localhost;dbname=nasoremodel', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->exec("SET NAMES 'utf8'");										
-?>

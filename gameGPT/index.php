51224399f6bb206481092861ea5f716a3d2fb7bd
jason naso <jasonnaso@jasons-MacBook-Air.local>
2015-12-24 01:24:07 +0100

    Push index and game

---
+++ gameGPT/index.php
@@ -0,0 +1,44 @@
+<!doctype html> 
+<html lang="en"> 
+<head> 
+	<meta charset="UTF-8"/>
+	<title>gameGpt</title>
+
+	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<div class="game-container">
+		<div class="overlay">
+			<div class="opening-overlay-text">Welcome to Get Paid To's Ready Aim Fire! Here's how it works. Push the start game or space bar and the gun site will move horizontally. When the site is in a position you like, push the Set Aim button or space bar and the site will then move vertically. Press the Fire Button or space bar when the site is over one of the three targets.  If you hit all three you win!!!</div>
+			<div class="good-luck">Good Luck!</div>
+ 			<input type="image" src="assets/images/btnStartGame.png"  class="btn-start"  id="startGame" />
+ 		</div>
+
+		<div id="gptGame"></div>
+
+		<div class="timed-out-overlay">
+			<div class="overlay-text">Timed Out</div>
+			<div class="overlay-text">Banner</div>
+		</div>
+
+		<div class="winner-overlay">
+			<div class="overlay-text">Winner!  Nice Shooting!</div>
+			<div class="overlay-text">Banner</div>
+		</div>
+		<div class="loser-overlay">
+			<div class="overlay-text">Sorry You needed all three.</div>
+			<div class="overlay-text">Banner</div>
+		</div>
+ 		
+ 	</div>
+
+
+	<script type="text/javascript" src="js/phaser.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
+

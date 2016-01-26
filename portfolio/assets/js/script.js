ae12b8578c74262cea925989c488239e65000dbb
jason naso <jasonnaso@jasons-MacBook-Air.local>
2016-01-26 09:15:20 +0100

    Update For Images

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -27,28 +27,39 @@ $(function() {
 
 var ctx = document.querySelector("canvas").getContext("2d"),
     dashLen = 220, dashOffset = dashLen, speed = 10,
-    txt = "Jason  Naso ", x = 30, i = 0;
-
-ctx.font = "100px Orator Std,Arial,sans-serif"; 
-ctx.lineWidth = 3; ctx.lineJoin = "round"; ctx.globalAlpha = 2/3;
+    txt = "Jason  Naso ", x = 200, i = 0;
+ctx.font = "70px Play, Amatic SC   "; 
+ctx.lineWidth = 2; ctx.lineJoin = "round";
 ctx.strokeStyle = ctx.fillStyle = "#68a8ad";
 
 (function loop() {
-  ctx.clearRect(x, 0, 60, 150);
+  ctx.clearRect(x, 0, 200, 150);
   ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
   dashOffset -= speed;                                         // reduce dash length
-  ctx.strokeText(txt[i], x, 90);                               // stroke letter
+  ctx.strokeText(txt[i], x, 150);                               // stroke letter
 
   if (dashOffset > 0) requestAnimationFrame(loop);             // animate
   else {
-    ctx.fillText(txt[i], x, 90);                               // fill final letter
+    ctx.fillText(txt[i], x, 150);                               // fill final letter
     dashOffset = dashLen;                                      // prep next char
     x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
-    ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
-    ctx.rotate(Math.random() * 0.005);                         // random rotation
+    // ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
+    // ctx.rotate(Math.random() * 0.005);                         // random rotation
     if (i < txt.length) requestAnimationFrame(loop);
   }
 })();
+
+
+
+    $('.logo-naso').hover(function() {
+
+        $(this).addClass('transition');
+ 
+    }, function() {
+      
+        $(this).removeClass('transition');
+    });
+
 
 
 

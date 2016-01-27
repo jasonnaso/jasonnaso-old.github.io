c66829e2075501a7780a7ae828bfef75d77e8ee1
jason naso <jasonnaso@jasons-MacBook-Air.local>
2016-01-27 16:56:28 +0100

    Portfolio push

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -1,71 +1,138 @@
-move();
-function move (){
-console.log("here");
-$(".hello").animate({left: '250px'});
+$(function() {
+
+    (function init(){
+
+        drawName();
+
+
+    })();
+
+    function drawName(){
+
+        //Canvas for name drawing
+        var ctx = document.querySelector("canvas").getContext("2d");
+        var dashLen = 220;
+        var dashOffset = dashLen; 
+        var speed = 10;
+        var txt = "Jason  Naso "; 
+        var x = 200; 
+        var i = 0;
+        
+        ctx.font = "70px Play, Amatic SC   "; 
+        ctx.lineWidth = 2; 
+        ctx.lineJoin = "round";
+        ctx.strokeStyle = ctx.fillStyle = "#68a8ad";
+
+        (function loop() {
+            
+            ctx.clearRect(x, 0, 200, 150);
+            ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+            dashOffset -= speed;                                         // reduce dash length
+            ctx.strokeText(txt[i], x, 150);                               // stroke letter
+
+            if (dashOffset > 0) requestAnimationFrame(loop);             // animate
+            
+            else {
+                
+                ctx.fillText(txt[i], x, 150);                               // fill final letter
+                dashOffset = dashLen;                                      // prep next char
+                x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+                // ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
+                // ctx.rotate(Math.random() * 0.005);                         // random rotation
+                if (i < txt.length) requestAnimationFrame(loop);
+            }
+        })();
+    }
+
+        $('.all-img').hover(function() {
+
+            $(this).addClass('transition');
+
+            }, 
+
+            function() {
+
+                $(this).removeClass('transition');
+            });
+
+    //Draw Line
+    function drawLine(topLine, bottomLine){
+        $(".border").hide();
+
+        $(topLine).show();
+        $(topLine).animate({width: "100%"},1000, function() {
+
+            $(topLine).animate({width: "0%"},1000);
+            $(bottomLine).show();
+            $(bottomLine).animate({width: "100%"},1000, function() {
+
+                $(bottomLine).animate({width: "0%"},1000);
+          
+        });
+    });
 
 }
+    
+    var testScroll = (function() {
+        console.log("Testscroll called");
+        var executed = false;
 
-$(function() {
-    $(".border, #content").hide();
-    $("#topbar").show();
-    $("#topbar").animate({width: "100%"},1000, function() {
-      $("#rightbar").show();
-      $("#rightbar").animate({height: "100%"},1000, function() {
-        $("#bottombar").show();
-        $("#bottombar").animate({width: "100%"},1000, function() {
-          $("#leftbar").show();
-          $("#leftbar").animate({height: "100%"},1000, function() {
-            $("#content").fadeIn(1000);
-          });
-        });
-      });
-    });
-  });
+        return function () {
 
+            if (!executed && window.pageYOffset > 600) {
 
+              
+                
 
+                executed = true;
+                
+                // $(bottomLine).animate({width: "0%"},1000);
 
-var ctx = document.querySelector("canvas").getContext("2d"),
-    dashLen = 220, dashOffset = dashLen, speed = 10,
-    txt = "Jason  Naso ", x = 200, i = 0;
-ctx.font = "70px Play, Amatic SC   "; 
-ctx.lineWidth = 2; ctx.lineJoin = "round";
-ctx.strokeStyle = ctx.fillStyle = "#68a8ad";
-
-(function loop() {
-  ctx.clearRect(x, 0, 200, 150);
-  ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
-  dashOffset -= speed;                                         // reduce dash length
-  ctx.strokeText(txt[i], x, 150);                               // stroke letter
-
-  if (dashOffset > 0) requestAnimationFrame(loop);             // animate
-  else {
-    ctx.fillText(txt[i], x, 150);                               // fill final letter
-    dashOffset = dashLen;                                      // prep next char
-    x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
-    // ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
-    // ctx.rotate(Math.random() * 0.005);                         // random rotation
-    if (i < txt.length) requestAnimationFrame(loop);
-  }
-})();
+                var topLine = "#lineThree";
+                var bottomLine = "#lineFour";
+                drawLine(topLine, bottomLine);
+            }
+        };
+    })();
+    window.onscroll = testScroll;
+    //http://stackoverflow.com/questions/5672320/trigger-events-when-the-window-is-scrolled-to-certain-positions
+    //http://stackoverflow.com/questions/12713564/function-in-javascript-that-can-be-called-only-once
+    var topLine = "#lineOne";
+    var bottomLine = "#lineTwo";
+    drawLine(topLine, bottomLine);
 
 
 
-    $('.all-img').hover(function() {
 
-        $(this).addClass('transition');
- 
-    }, function() {
-      
-        $(this).removeClass('transition');
-    });
+});
+
+
+  
 
 
 
 
 
 
+//Draw Line
+//$(".border, #content").hide();
+// $("#topbar").show();
 
+// $("#topbar").animate({width: "100%"},1000, function() {
+//     $("#rightbar").show();
+  
+//     $("#rightbar").animate({height: "100%"},1000, function() {
+//         $("#bottombar").show();
+
+//         $("#bottombar").animate({width: "100%"},1000, function() {
+//             $("#leftbar").show();
+            
+//             $("#leftbar").animate({height: "100%"},1000, function() {
+//                 $("#content").fadeIn(1000);
+//             });
+//         });
+//     });
+// });
 
 
 
